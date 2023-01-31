c56b1c7f7a8ee08181b385c9afa889d366afd055
Sumilang Plucena <splucena2011@gmail.com>
2023-01-31 13:13:37 +0000

    Finish custom User model

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Tag, Post
+from blog.models import Tag, Post, Comment, AuthorProfile
 # Register your models here.
 
 admin.site.register(Tag)
@@ -9,3 +9,7 @@ class PostAdmin(admin.ModelAdmin):
   list_display = ('slug', 'published_at')
 
 admin.site.register(Post, PostAdmin)
+
+admin.site.register(Comment)
+
+admin.site.register(AuthorProfile)
